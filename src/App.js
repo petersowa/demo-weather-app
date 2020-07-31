e76a15af34407e80914e454dff10a0222aa6fe14
petersowa <petersowa@users.noreply.github.com>
2020-07-31 18:10:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,323 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { getDefaultNormalizer } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = {
+	States: {
+		IL: {
+			currentdate: '04/05/2019',
+			time: '02:59 PM',
+			cities: [
+				{
+					name: 'Chicago',
+					forecast: [
+						{
+							Date: '04/05/2019',
+							Time: '2.59pm',
+							temprature: 47,
+							feels: 40,
+						},
+						{
+							Date: '04/06/2019',
+							Time: '2.59pm',
+							temprature: 57,
+							feels: 55,
+						},
+						{
+							Date: '04/07/2019',
+							Time: '2.59pm',
+							temprature: 45,
+							feels: 44,
+						},
+						{
+							Date: '04/08/2019',
+							Time: '2.59pm',
+							temprature: 61,
+							feels: 50,
+						},
+						{
+							Date: '04/09/2019',
+							Time: '2.59pm',
+							temprature: 68,
+							feels: 65,
+						},
+					],
+				},
+				{
+					name: 'Naperville',
+					forecast: [
+						{
+							Date: '04/05/2019',
+							Time: '2.59pm',
+							temprature: 47,
+							feels: 40,
+						},
+						{
+							Date: '04/06/2019',
+							Time: '2.59pm',
+							temprature: 59,
+							feels: 54,
+						},
+						{
+							Date: '04/07/2019',
+							Time: '2.59pm',
+							temprature: 47,
+							feels: 46,
+						},
+						{
+							Date: '04/08/2019',
+							Time: '2.59pm',
+							temprature: 63,
+							feels: 62,
+						},
+						{
+							Date: '04/09/2019',
+							Time: '2.59pm',
+							temprature: 70,
+							feels: 68,
+						},
+					],
+				},
+			],
+		},
+		NY: {
+			currentdate: '04/05/2019',
+			time: '02:59 PM',
+			cities: [
+				{
+					name: 'New York',
+					forecast: [
+						{
+							Date: '04/05/2019',
+							Time: '2.59pm',
+							temprature: 57,
+							feels: 50,
+						},
+						{
+							Date: '04/06/2019',
+							Time: '2.59pm',
+							temprature: 67,
+							feels: 65,
+						},
+						{
+							Date: '04/07/2019',
+							Time: '2.59pm',
+							temprature: 55,
+							feels: 54,
+						},
+						{
+							Date: '04/08/2019',
+							Time: '2.59pm',
+							temprature: 71,
+							feels: 60,
+						},
+						{
+							Date: '04/09/2019',
+							Time: '2.59pm',
+							temprature: 78,
+							feels: 75,
+						},
+					],
+				},
+				{
+					name: 'Buffalo',
+					forecast: [
+						{
+							Date: '04/05/2019',
+							Time: '2.59pm',
+							temprature: 58,
+							feels: 40,
+						},
+						{
+							Date: '04/06/2019',
+							Time: '2.59pm',
+							temprature: 68,
+							feels: 64,
+						},
+						{
+							Date: '04/07/2019',
+							Time: '2.59pm',
+							temprature: 57,
+							feels: 46,
+						},
+						{
+							Date: '04/08/2019',
+							Time: '2.59pm',
+							temprature: 63,
+							feels: 62,
+						},
+						{
+							Date: '04/09/2019',
+							Time: '2.59pm',
+							temprature: 40,
+							feels: 48,
+						},
+					],
+				},
+			],
+		},
+		CA: {
+			currentdate: '04/05/2019',
+			time: '02:59 PM',
+			cities: [
+				{
+					name: 'Los Angeles',
+					forecast: [
+						{
+							Date: '04/05/2019',
+							Time: '2.59pm',
+							temprature: 67,
+							feels: 50,
+						},
+						{
+							Date: '04/06/2019',
+							Time: '2.59pm',
+							temprature: 77,
+							feels: 65,
+						},
+						{
+							Date: '04/07/2019',
+							Time: '2.59pm',
+							temprature: 65,
+							feels: 54,
+						},
+						{
+							Date: '04/08/2019',
+							Time: '2.59pm',
+							temprature: 71,
+							feels: 60,
+						},
+						{
+							Date: '04/09/2019',
+							Time: '2.59pm',
+							temprature: 78,
+							feels: 75,
+						},
+					],
+				},
+				{
+					name: 'San francisco',
+					forecast: [
+						{
+							Date: '04/05/2019',
+							Time: '2.59pm',
+							temprature: 68,
+							feels: 50,
+						},
+						{
+							Date: '04/06/2019',
+							Time: '2.59pm',
+							temprature: 78,
+							feels: 64,
+						},
+						{
+							Date: '04/07/2019',
+							Time: '2.59pm',
+							temprature: 67,
+							feels: 66,
+						},
+						{
+							Date: '04/08/2019',
+							Time: '2.59pm',
+							temprature: 73,
+							feels: 72,
+						},
+						{
+							Date: '04/09/2019',
+							Time: '2.59pm',
+							temprature: 70,
+							feels: 68,
+						},
+					],
+				},
+			],
+		},
+	},
+};
+
+function ExtendedForecast(props) {
+	return <div>extended</div>;
 }
 
-export default App;
+function WeatherInfo(props) {
+	const [showExtended, setShowExtended] = useState(false);
+	const currentForecast = props.city.forecast.forecast[0];
+
+	console.log(currentForecast);
+	return (
+		<div>
+			<h2>Weather data for {props.city.name}</h2>
+			<p>Current Temperature: {currentForecast.temprature}</p>
+			<p>Feels Like: {currentForecast.feels}</p>
+			<button onClick={() => setShowExtended(!showExtended)}>
+				Extended Forecast
+			</button>
+			{showExtended && <ExtendedForecast></ExtendedForecast>}
+		</div>
+	);
+}
+
+class GetCityForm extends React.Component {
+	state = {
+		cityName: '',
+	};
+
+	render() {
+		return (
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					if (this.state.cityName !== '') {
+						this.props.getWeatherInfo(this.state.cityName);
+					}
+					this.setState({ cityName: '' });
+				}}
+			>
+				<label>City Name:</label>
+				<input
+					type="text"
+					value={this.state.cityName}
+					onChange={(e) => {
+						this.setState({ cityName: e.target.value });
+					}}
+				></input>
+			</form>
+		);
+	}
+}
+
+class WeatherApp extends React.Component {
+	state = {
+		cityInfo: null,
+	};
+
+	getWeatherData(cityName) {
+		for (const state in data.States) {
+			const result = data.States[state].cities.find(
+				(city) => city.name === cityName
+			);
+			if (result) {
+				this.setState({
+					cityInfo: { name: cityName, forecast: result },
+				});
+			}
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Weather App</h2>
+				<GetCityForm
+					getWeatherInfo={(city) => {
+						this.getWeatherData(city);
+					}}
+				></GetCityForm>
+				{this.state.cityInfo && (
+					<WeatherInfo city={this.state.cityInfo}></WeatherInfo>
+				)}
+			</div>
+		);
+	}
+}
+
+export default WeatherApp;
